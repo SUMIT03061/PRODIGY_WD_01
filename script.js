b1254d0371a1d5391a8de4922f2905af035010de
SUMIT03061 <sumitmall0306@gmail.com>
2025-08-19 22:13:28 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Navbar toggle for mobile
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
